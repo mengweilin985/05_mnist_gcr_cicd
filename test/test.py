072de7d47019a7cfc27bddd92414fff980cf08ec
mengweilin985 <61502637+mengweilin985@users.noreply.github.com>
2024-07-04 10:22:05 +0800

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,5 +1,5 @@
 import requests
 
-resp = requests.post("https://ml-run-service-h6j65vuzbq-as.a.run.app", files={'file': open('eight.png', 'rb')})
+resp = requests.post("https://ml-run-service-h6j65vuzbq-as.a.run.app", files={'file': open('three.png', 'rb')})
 
 print(resp.json())
